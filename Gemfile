019e99a52b481f9fbab4bf85bdbbedc96fd9755a
Yoshiyuki Sugimoto <s.yosiyuki@gmail.com>
2014-06-07 17:58:56 +0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,33 @@
+# vim: filetype=ruby
+source 'https://rubygems.org'
+ruby '2.1.2'
+
+gem 'rails', '4.0.1'
+gem 'rails-i18n'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'bootstrap-sass', '~> 3.0.2.0'
+gem 'slim-rails'
+gem 'turbolinks'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'unicorn'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'factory_girl'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+  gem 'accept_values_for'
+  gem 'turn', '~> 0.8.3', :require => false
+  gem 'email_spec'
+end
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+XephangFacebook::Application.config.secret_key_base = '962c56b591a0d0db8c0e9d2bb09ba615981c52fb0bf3c1c3303ebec77fb3525677cb61c4daf42cb646f767f315ed733341db7b89b7c4cb1f530da20fe98bc5dd'
